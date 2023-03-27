a2401866dd952b2c342c1f3ac29efeee867870f4
SArthurX <sxa05z@gmail.com>
2023-03-28 04:08:35 +0800

    first commit

--- ontt.py
+++ ontt.py
@@ -1,69 +1,140 @@
-import time
 import cv2
-import os
-import HandTrackingModule as htm
- 
-wCam, hCam = 640, 480
- 
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
- 
-folderPath = "FingerImages"
-myList = os.listdir(folderPath)
-print(myList)
-overlayList = []
-for imPath in myList:
-    image = cv2.imread(f'{folderPath}/{imPath}')
-    # print(f'{folderPath}/{imPath}')
-    overlayList.append(image)
- 
-print(len(overlayList))
-pTime = 0
- 
-detector = htm.handDetector(detectionCon=0.75)
- 
-tipIds = [4, 8, 12, 16, 20]
- 
-while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-    # print(lmList)
- 
-    if len(lmList) != 0:
-        fingers = []
- 
-        # Thumb
-        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
- 
-        # 4 Fingers
-        for id in range(1, 5):
-            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
- 
-        # print(fingers)
-        totalFingers = fingers.count(1)
-        print(totalFingers)
- 
-        h, w, c = overlayList[totalFingers - 1].shape
-        img[0:h, 0:w] = overlayList[totalFingers - 1]
- 
-        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
-                    10, (255, 0, 0), 25)
- 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
- 
-    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
-                3, (255, 0, 0), 3)
- 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+import mediapipe as mp
+import math
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# 根據兩點的座標，計算角度
+def vector_2d_angle(v1, v2):
+    v1_x = v1[0]
+    v1_y = v1[1]
+    v2_x = v2[0]
+    v2_y = v2[1]
+    try:
+        angle_= math.degrees(math.acos((v1_x*v2_x+v1_y*v2_y)/(((v1_x**2+v1_y**2)**0.5)*((v2_x**2+v2_y**2)**0.5))))
+    except:
+        angle_ = 180
+    return angle_
+
+# 根據傳入的 21 個節點座標，得到該手指的角度
+def hand_angle(hand_):
+    angle_list = []
+    # thumb 大拇指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[2][0])),(int(hand_[0][1])-int(hand_[2][1]))),
+        ((int(hand_[3][0])- int(hand_[4][0])),(int(hand_[3][1])- int(hand_[4][1])))
+        )
+    angle_list.append(angle_)
+    # index 食指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])-int(hand_[6][0])),(int(hand_[0][1])- int(hand_[6][1]))),
+        ((int(hand_[7][0])- int(hand_[8][0])),(int(hand_[7][1])- int(hand_[8][1])))
+        )
+    angle_list.append(angle_)
+    # middle 中指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[10][0])),(int(hand_[0][1])- int(hand_[10][1]))),
+        ((int(hand_[11][0])- int(hand_[12][0])),(int(hand_[11][1])- int(hand_[12][1])))
+        )
+    angle_list.append(angle_)
+    # ring 無名指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[14][0])),(int(hand_[0][1])- int(hand_[14][1]))),
+        ((int(hand_[15][0])- int(hand_[16][0])),(int(hand_[15][1])- int(hand_[16][1])))
+        )
+    angle_list.append(angle_)
+    # pink 小拇指角度
+    angle_ = vector_2d_angle(
+        ((int(hand_[0][0])- int(hand_[18][0])),(int(hand_[0][1])- int(hand_[18][1]))),
+        ((int(hand_[19][0])- int(hand_[20][0])),(int(hand_[19][1])- int(hand_[20][1])))
+        )
+    angle_list.append(angle_)
+    return angle_list
+
+# 根據手指角度的串列內容，返回對應的手勢名稱
+def hand_pos(finger_angle):
+    f1 = finger_angle[0]   # 大拇指角度
+    f2 = finger_angle[1]   # 食指角度
+    f3 = finger_angle[2]   # 中指角度
+    f4 = finger_angle[3]   # 無名指角度
+    f5 = finger_angle[4]   # 小拇指角度
+
+    # 小於 50 表示手指伸直，大於等於 50 表示手指捲縮
+    if f1<50 and f2>=50 and f3>=50 and f4>=50 and f5>=50:
+        return 'good'
+    elif f1>=50 and f2>=50 and f3<50 and f4>=50 and f5>=50:
+        return 'FUCK!'
+    elif f1<50 and f2<50 and f3>=50 and f4>=50 and f5<50:
+        return 'ROCK!'
+    elif f1>=50 and f2>=50 and f3>=50 and f4>=50 and f5>=50:
+        return '0'
+    elif f1>=50 and f2>=50 and f3>=50 and f4>=50 and f5<50:
+        return 'pink'
+    elif f1>=50 and f2<50 and f3>=50 and f4>=50 and f5>=50:
+        return '1'
+    elif f1>=50 and f2<50 and f3<50 and f4>=50 and f5>=50:
+        return '2'
+    elif f1>=50 and f2>=50 and f3<50 and f4<50 and f5<50:
+        return 'ok'
+    elif f1<50 and f2>=50 and f3<50 and f4<50 and f5<50:
+        return 'ok'
+    elif f1>=50 and f2<50 and f3<50 and f4<50 and f5>50:
+        return '3'
+    elif f1>=50 and f2<50 and f3<50 and f4<50 and f5<50:
+        return '4'
+    elif f1<50 and f2<50 and f3<50 and f4<50 and f5<50:
+        return '5'
+    elif f1<50 and f2>=50 and f3>=50 and f4>=50 and f5<50:
+        return '6'
+    elif f1<50 and f2<50 and f3>=50 and f4>=50 and f5>=50:
+        return '7'
+    elif f1<50 and f2<50 and f3<50 and f4>=50 and f5>=50:
+        return '8'
+    elif f1<50 and f2<50 and f3<50 and f4<50 and f5>=50:
+        return '9'
+    else:
+        return ''
+
+cap = cv2.VideoCapture(0)            # 讀取攝影機
+fontFace = cv2.FONT_HERSHEY_SIMPLEX  # 印出文字的字型
+lineType = cv2.LINE_AA               # 印出文字的邊框
+
+# mediapipe 啟用偵測手掌
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    w, h = 640, 480                                  # 影像尺寸
+    while True:
+        ret, img = cap.read()
+        img = cv2.flip(img, 1)                       #鏡像翻轉
+        if not ret:
+            print("Cannot receive frame")
+            break
+        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 轉換成 RGB 色彩
+        results = hands.process(img2)                # 偵測手勢
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                finger_points = []                   # 記錄手指節點座標的串列
+                for i in hand_landmarks.landmark:
+                    # 將 21 個節點換算成座標，記錄到 finger_points
+                    x = i.x*w
+                    y = i.y*h
+                    finger_points.append((x,y))
+                if finger_points:
+                    finger_angle = hand_angle(finger_points) # 計算手指角度，回傳長度為 5 的串列
+                    #print(finger_angle)                     # 印出角度 ( 有需要就開啟註解 )
+                    text = hand_pos(finger_angle)            # 取得手勢所回傳的內容
+                    cv2.putText(img, text, (30,120), fontFace, 5, (255,255,255), 10, lineType) # 印出文字
+                    
+        cv2.imshow('oxxostudio', img)       
+        if cv2.waitKey(5) == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
